7384abbf1c30dd4ed46d83c2da5ba4f11a3a2a37
Alfonso Egio <alfonso.egio@gmail.com>
2016-05-26 09:09:03 +0200

    Initial commit

---
+++ commit.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $name = shift @ARGV || die "\nLack of commit name\n\n";
+
+foreach my $arg ( @ARGV ) {
+  $name .= " $arg";
+}
+
+system("git add commit.pl");
+
+system("git commit -m '$name'\n");
