ab68db77c4233c9d15ce732450c8c1f305ceb334
Alfonso Egio <alfonso.egio@gmail.com>
2016-05-26 11:56:57 +0200

    body-parser bug

--- freiworld.js
+++ freiworld.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 var fortune = require('./lib/fortune.js');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -8,17 +9,56 @@ app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
 
+app.set('env','development');
+
+app.disable('x-powered-by');
+
+app.use(bodyParser.json());                        
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(function(req,res,next) {
     res.locals.showTests = (app.get('env') !== 'production') && (req.query.test === '1');
     next();
 });
 
+app.get('/headers',function(req,res) {
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
 app.get('/',function(req,res) {
-    res.render('home');
+    res.render('home',{"title":"Freiworld homepage"});
 });
 
 app.get('/about',function(req,res) {
-    res.render('about');
+    res.render('about',{
+	title: "About Freiworld",
+	fortune: fortune.getFortune(),
+	pageTestScript: "/qa/about-tests.js"
+    });
+});
+
+app.post('/retrieve-video-list', function(req,res) {
+    res.set('Content-Type','text/plain');
+    var s = '';
+    for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    s += req.body.name;
+    console.log('Received video list query for '+ req.body.name + ' <' + req.body.email + '>');
+    res.send(s);
+});
+
+app.get('/categories/porn', function(req,res) {
+    res.render('categories/porn',{
+	title: "Porn category"
+    });
+});
+
+app.get('/categories/video-list', function(req,res) {
+    res.render('categories/video-list',{
+	title: "Video list"
+    });
 });
 
 app.use(express.static(__dirname+'/public'));
@@ -38,4 +78,5 @@ app.listen(app.get('port'),function() {
     console.log('Express started on http://localhost'+app.get('port')+'; Press Ctrl-C to terminate.');
 });
 
-console.log(fortune.getFortune())
+console.log(fortune.getFortune());
+
