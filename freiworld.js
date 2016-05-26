e028c684c95afc7244f4fca90982f42f132c1c13
Alfonso Egio <alfonso.egio@gmail.com>
2016-05-26 10:16:36 +0200

    Global tests

--- freiworld.js
+++ freiworld.js
@@ -8,6 +8,11 @@ app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req,res,next) {
+    res.locals.showTests = (app.get('env') !== 'production') && (req.query.test === '1');
+    next();
+});
+
 app.get('/',function(req,res) {
     res.render('home');
 });
