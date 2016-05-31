7ae12bc1260715c566e6df9527c67d38576754e3
freising <freising@nemox.(none)>
2016-05-31 10:21:22 +0200

    error in package

--- freiworld.js
+++ freiworld.js
@@ -127,6 +127,8 @@ app.use(function(req,res,next) {
     res.render('500');
 });
 
+console.log("TRACA = " +app.get('port'));
+
 app.listen(app.get('port'),server_ip_address,function() {
     console.log('Express started on http://localhost'+app.get('port')+'; Press Ctrl-C to terminate.');
 });
